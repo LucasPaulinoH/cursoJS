bcfde6ea2b560ceee4c55dd66f7ec696d180dd41
Lucas Paulino <87985909+LucasPaulinoH@users.noreply.github.com>
2022-06-25 11:35:47 -0300

    adição da aula4

---
+++ Aula4/script.js
@@ -0,0 +1,4 @@
+window.alert('Olá!') //alerta na tela com botão OK
+window.confirm('Seja bem-vindo!'); //alerta na tela com botão OK e cancelar
+window.prompt('Para começar, digite seu nome: '); //solicita input do teclado
+
