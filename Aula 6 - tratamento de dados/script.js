9b0cec12efc6388c63576051a8f79bc4cba4149f
Lucas Paulino <lucashsp12@hotmail.com>
2022-06-29 11:34:44 -0300

    adição da aula 7/8

--- Aula 6 - tratamento de dados/script.js
+++ Aula 6 - tratamento de dados/script.js
@@ -4,7 +4,14 @@ var media = (n1 + n2)/2;
 
 // obs.: .toString() ou String() converte o parâmetro para String
 
-window.alert(`A média entre ${n1} e ${n2} é ${media}`); 
+window.alert(`A média entre ${n1} e ${n2} é ${media}`); //placeholder:`${variável}`
 
-//placeholder:`${variável}`
+media.toFixed(2).replace('.',','); /* mostra a precisão de duas casas decimais, e o replace
+substitui um elemento por outro, nesse caso o . pela ,*/
+
+window.alert(`Esta é a média com a precisão de duas casas: ${media.toFixed(2)}`);
+
+window.alert(media.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})); //formata o número para real
+
+
 
