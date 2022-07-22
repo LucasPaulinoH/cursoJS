90a4a8f4052d5eb9279de057d583acbc3238fe66
Lucas Paulino <lucashsp12@hotmail.com>
2022-07-22 10:14:10 -0300

    adição da exemplo sobre arrow functions

---
+++ Aula 14 - funções/arrowFunction.js
@@ -0,0 +1,6 @@
+const sum = (a = 0, b = 0) =>{
+  return a+b;
+};
+
+const sumValue = sum(5,7);
+console.log(sumValue);
---
+++ Aula 5 - variáveis/truthy&falsy.js
@@ -0,0 +1,22 @@
+/* as seguintes situações são false se em 
+  um contexto booleano: */
+
+const x = "";
+
+const y = 0;
+
+const a = null;
+
+const b = undefined;
+
+console.log(`${!!x}, ${!!y}, ${!!a}, ${!!b}`); /* os dois booleanos
+de negação (!!) testam o valor booleano da variável, enquanto o booleano
+(!) representa uma negação simples */
+
+/* as seguintes situações são true se em
+   um contexto booleano: */
+
+const list = [];
+const obj = {}
+
+console.log(`${!!list}, ${!!obj}, ${!!a}, ${!!b}`);
